23edccfd1908f8cd2e7bb5aaa6625405ec58f5c9
sammtcbn <sammtcbn@gmail.com>
2021-03-04 01:53:16 +0800

    first draft

---
+++ 99-all-app.bat
@@ -0,0 +1,5 @@
+C:\sandbox_share\npp-installer.exe /S
+C:\sandbox_share\7z-installer.exe /S
+C:\sandbox_share\brave-installer.exe --install --silent --system-level
+C:\sandbox_share\git-installer.exe /S
+mkdir C:\samdata

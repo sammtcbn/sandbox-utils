980b0ea1e642b2901251f790499cdd9f00c1148c
sammtcbn <sammtcbn@gmail.com>
2021-03-04 02:00:01 +0800

    silent install git

--- 99-all-app.bat
+++ 99-all-app.bat
@@ -1,5 +1,5 @@
 C:\sandbox_share\npp-installer.exe /S
 C:\sandbox_share\7z-installer.exe /S
 C:\sandbox_share\brave-installer.exe --install --silent --system-level
-C:\sandbox_share\git-installer.exe /S
+C:\sandbox_share\git-installer.exe /VERYSILENT
 mkdir C:\samdata

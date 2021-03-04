c2b47c0f8c55be5d5a2e2a3d6711254320a07325
sammtcbn <sammtcbn@gmail.com>
2021-03-04 19:17:23 +0800

    install app by chocolatey

---
+++ 90-app-by-chocolatey.bat
@@ -0,0 +1,5 @@
+call C:\sandbox-utils\01-init.bat
+call C:\sandbox-utils\wait-network-ready.bat
+call C:\sandbox-utils\install-chocolatey.bat
+choco install notepadplusplus -y >> C:\Users\WDAGUtilityAccount\Desktop\install-log.txt
+call C:\sandbox-utils\generate-boot-ok.bat

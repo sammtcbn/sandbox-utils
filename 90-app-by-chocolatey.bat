d742e5f31f1101b6bc583999db209ae1ca7836f6
sammtcbn <sammtcbn@gmail.com>
2021-03-05 00:11:39 +0800

    boxstarter

--- 90-app-by-chocolatey.bat
+++ 90-app-by-chocolatey.bat
@@ -1,5 +1,5 @@
 call C:\sandbox-utils\01-init.bat
 call C:\sandbox-utils\wait-network-ready.bat
 call C:\sandbox-utils\install-chocolatey.bat
-choco install notepadplusplus 7zip -y >> C:\Users\WDAGUtilityAccount\Desktop\install-log.txt
+choco install notepadplusplus 7zip -y >> C:\Users\WDAGUtilityAccount\Desktop\chocolatey-log.txt
 call C:\sandbox-utils\generate-boot-ok.bat

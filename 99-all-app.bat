7356efd171447edea7682ad22266c31ddbb9b955
sammtcbn <sammtcbn@gmail.com>
2021-03-04 10:04:49 +0800

    change hostfolder name

--- 99-all-app.bat
+++ 99-all-app.bat
@@ -1,5 +1,5 @@
-C:\sandbox_share\npp-installer.exe /S
-C:\sandbox_share\7z-installer.exe /S
-C:\sandbox_share\brave-installer.exe --install --silent --system-level
-C:\sandbox_share\git-installer.exe /VERYSILENT
+C:\sandbox-utils\npp-installer.exe /S
+C:\sandbox-utils\7z-installer.exe /S
+C:\sandbox-utils\brave-installer.exe --install --silent --system-level
+C:\sandbox-utils\git-installer.exe /VERYSILENT
 mkdir C:\samdata

77ce1da3a57dd38d8710c3c40802ed0e42ef74e2
sammtcbn <sammtcbn@gmail.com>
2021-03-05 15:15:03 +0800

    add nodejs dev sandbox

---
+++ multiapp-wsb/nodejs-dev-init.bat
@@ -0,0 +1,4 @@
+call C:\sandbox-utils\app-script\7zip-download.bat
+call C:\sandbox-utils\app-script\npp-download.bat
+call C:\sandbox-utils\app-script\git-download.bat
+call C:\sandbox-utils\app-script\nodejs-download.bat
---
+++ multiapp-wsb/nodejs-dev.bat
@@ -0,0 +1,7 @@
+call C:\sandbox-utils\utils\init.bat
+call C:\sandbox-utils\utils\wait-network-ready.bat
+call C:\sandbox-utils\app-script\7zip-install.bat
+call C:\sandbox-utils\app-script\npp-install.bat
+call C:\sandbox-utils\app-script\git-install.bat
+call C:\sandbox-utils\app-script\nodejs-install.bat
+call C:\sandbox-utils\utils\postaction.bat

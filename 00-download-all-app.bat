8695a3cb6d3bed8a5d050b1e7d548167173e36da
sammtcbn <sammtcbn@gmail.com>
2021-03-05 13:27:34 +0800

    remove useless files

--- 00-download-all-app.bat
+++ 00-download-all-app.bat
@@ -1,7 +1,2 @@
-curl --output npp-installer.exe -L https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.3/npp.7.9.3.Installer.x64.exe
-
 curl --output vc_redist-2015-2019.x64.exe -L https://aka.ms/vs/16/release/vc_redist.x64.exe
-
 curl --output mosquitto-installer.exe https://mosquitto.org/files/binary/win64/mosquitto-2.0.8-install-windows-x64.exe
-
-curl --output ChromeSetup.exe -L http://dl.google.com/chrome/install/latest/chrome_installer.exe
--- 99-all-app.bat
+++
@@ -1,6 +0,0 @@
-C:\sandbox-utils\npp-installer.exe /S
-C:\sandbox-utils\7z-installer.exe /S
-C:\sandbox-utils\brave-installer.exe --install --silent --system-level
-C:\sandbox-utils\git-installer.exe /VERYSILENT
-C:\sandbox-utils\node-installer.msi /quiet
-mkdir C:\samdata

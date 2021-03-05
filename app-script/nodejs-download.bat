4972644d0c7134eca5d40107e05e9d95d3428da8
sammtcbn <sammtcbn@gmail.com>
2021-03-05 15:27:36 +0800

    nodejs change to v12

--- app-script/nodejs-download.bat
+++ app-script/nodejs-download.bat
@@ -1,2 +1,7 @@
 mkdir C:\sandbox-dl
-curl --output C:\sandbox-dl\nodejs-installer.msi https://nodejs.org/dist/v14.16.0/node-v14.16.0-x64.msi
+
+@rem v12
+curl --output C:\sandbox-dl\nodejs-installer.msi https://nodejs.org/dist/v12.21.0/node-v12.21.0-x64.msi
+
+@rem v14
+@rem curl --output C:\sandbox-dl\nodejs-installer.msi https://nodejs.org/dist/v14.16.0/node-v14.16.0-x64.msi

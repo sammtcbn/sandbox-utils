3d8eed928aee069bf1828f01990c0de4919b9216
sammtcbn <sammtcbn@gmail.com>
2021-03-05 20:21:13 +0800

    not to open vscode after install vscode

--- app-script/vscode-install.bat
+++ app-script/vscode-install.bat
@@ -1,2 +1,3 @@
 @rem refer to https://docs.microsoft.com/en-us/windows/security/threat-protection/windows-sandbox/windows-sandbox-configure-using-wsb-file
-C:\sandbox-dl\vscode.exe /verysilent /suppressmsgboxes
+@rem refer to https://silentinstallhq.com/visual-studio-code-silent-install-how-to-guide/
+C:\sandbox-dl\vscode.exe /verysilent /suppressmsgboxes /NORESTART /MERGETASKS=!runcode

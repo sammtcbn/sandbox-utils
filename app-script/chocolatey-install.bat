9b4e648590903ca17986313f2b022a04102c7278
sammtcbn <sammtcbn@gmail.com>
2021-03-06 15:42:53 +0800

    re-org chocolatey sandbox

---
+++ app-script/chocolatey-install.bat
@@ -0,0 +1,2 @@
+@rem refer to https://docs.chocolatey.org/en-us/choco/setup
+start /wait powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
--- install-chocolatey.bat
+++
@@ -1,1 +0,0 @@
-start /wait powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"

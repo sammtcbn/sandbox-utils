cf2ce62cf0bec21fce4dabb2c892a0cffbabe2be
sammtcbn <sammtcbn@gmail.com>
2021-03-08 07:50:00 +0800

    create quiet sandbox using chocolatey

--- wsb-by-chocolatey/quiet.bat
+++ wsb-by-chocolatey/quiet.bat
@@ -2,5 +2,5 @@ call C:\sandbox-utils\utils\init.bat
 call C:\sandbox-utils\utils\wait-network-ready.bat
 call C:\sandbox-utils\app-script\chocolatey-install.bat
 choco install 7zip notepadplusplus brave k-litecodecpackfull vlc nircmd -y >> C:\Users\WDAGUtilityAccount\Desktop\chocolatey-log.txt
-call C:\sandbox-utils\app-script\nircmd-volume-highest.bat
+call C:\sandbox-utils\app-script\nircmd-mute.bat
 call C:\sandbox-utils\utils\postaction.bat

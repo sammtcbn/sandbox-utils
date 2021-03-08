f956991b007e60dc6f80f5cf81b04aeb44088671
sammtcbn <sammtcbn@gmail.com>
2021-03-08 15:21:37 +0800

    create a sandbox using chocolatey for development

---
+++ wsb-by-chocolatey/dev.bat
@@ -0,0 +1,18 @@
+call C:\sandbox-utils\utils\init.bat
+call C:\sandbox-utils\utils\wait-network-ready.bat
+call C:\sandbox-utils\app-script\chocolatey-install.bat
+
+@rem basic
+choco install -y 7zip notepadplusplus googlechrome >> C:\Users\WDAGUtilityAccount\Desktop\chocolatey-log.txt
+
+@rem network util
+choco install -y postman wireshark angryip >> C:\Users\WDAGUtilityAccount\Desktop\chocolatey-log.txt
+
+@rem development
+choco install -y git.install >> C:\Users\WDAGUtilityAccount\Desktop\chocolatey-log.txt
+choco install -y vscode >> C:\Users\WDAGUtilityAccount\Desktop\chocolatey-log.txt
+
+@rem NOTE: Need to call chocolatey-refreshenv.bat once before git clone project later in this batch file
+call C:\sandbox-utils\app-script\chocolatey-refreshenv.bat
+
+call C:\sandbox-utils\utils\postaction.bat

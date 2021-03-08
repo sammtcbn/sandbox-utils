7f486914e5bd405bd2e36dd7969b144c83e9347f
sammtcbn <sammtcbn@gmail.com>
2021-03-08 15:19:46 +0800

    add a script to refresh environment using chocolatey's script

---
+++ app-script/chocolatey-refreshenv.bat
@@ -0,0 +1,22 @@
+@echo off
+WHERE refreshenv
+if /I %ERRORLEVEL% NEQ 0 (
+
+    echo refreshenv not found in path >> C:\Users\WDAGUtilityAccount\Desktop\sandbox-utils-log.txt
+
+    if exist C:\ProgramData\chocolatey\bin\RefreshEnv.cmd (
+        @rem file exists
+        echo refreshenv found in chocolatey folder >> C:\Users\WDAGUtilityAccount\Desktop\sandbox-utils-log.txt
+        call C:\ProgramData\chocolatey\bin\RefreshEnv.cmd
+    ) else (
+        @rem file doesn't exist
+        echo refreshenv not found in chocolatey folder >> C:\Users\WDAGUtilityAccount\Desktop\sandbox-utils-log.txt
+        goto :EOF
+    )
+	
+) else (
+    echo refreshenv found in path >> C:\Users\WDAGUtilityAccount\Desktop\sandbox-utils-log.txt
+    refreshenv	
+)
+
+:EOF

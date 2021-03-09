057d00689df0e96dae5dbb442846cb357d3b14de
sammtcbn <sammtcbn@gmail.com>
2021-03-09 17:01:06 +0800

    update python dev sandbox

--- wsb-by-chocolatey/python-dev.bat
+++ wsb-by-chocolatey/python-dev.bat
@@ -1,10 +1,22 @@
 call C:\sandbox-utils\utils\init.bat
 call C:\sandbox-utils\utils\wait-network-ready.bat
 call C:\sandbox-utils\app-script\chocolatey-install.bat
+
+@rem basic
 choco install -y 7zip notepadplusplus googlechrome >> C:\sandbox-tmp\chocolatey-log.txt
 choco install -y git.install >> C:\sandbox-tmp\chocolatey-log.txt
+
+@rem python
 choco install -y python >> C:\sandbox-tmp\chocolatey-log.txt
 call C:\sandbox-utils\app-script\chocolatey-refreshenv.bat
-python -m pip install --upgrade pip >> C:\sandbox-tmp\python-pip-log.txt
+pip install --upgrade pip >> C:\sandbox-tmp\python-pip-log.txt
+
+@rem mongodb develop
+@rem pip install pymongo >> C:\sandbox-tmp\python-pip-log.txt
+@rem choco install -y robo3t >> C:\sandbox-tmp\chocolatey-log.txt
+
 call C:\sandbox-utils\app-script\sam-git-utils-install.bat
 call C:\sandbox-utils\utils\postaction.bat
+
+
+

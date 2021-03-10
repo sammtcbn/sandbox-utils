b4c43c7dced1f76aecd4d2bc85a271538ce476b6
sammtcbn <sammtcbn@gmail.com>
2021-03-10 19:00:26 +0800

    fix fail to install jq

--- app-script/jq-install-on-the-fly.bat
+++ app-script/jq-install-on-the-fly.bat
@@ -1,4 +1,5 @@
 @echo off
 mkdir C:\sandbox-tmp
 curl --output C:\sandbox-tmp\jq-win64.exe -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-win64.exe
-xcopy /Y C:\sandbox-tmp\jq-win64.exe C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps\jq.exe
+xcopy /Y /F C:\sandbox-tmp\jq-win64.exe C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps
+move /Y C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps\jq-win64.exe C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps\jq.exe

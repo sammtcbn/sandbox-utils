8c2e5eefd87b8773029fb61309ab842380e0d4c7
sammtcbn <sammtcbn@gmail.com>
2021-03-11 18:24:23 +0800

    minor update

--- app-script/azure-storage-explorer-install-on-the-fly.bat
+++ app-script/azure-storage-explorer-install-on-the-fly.bat
@@ -1,3 +1,3 @@
-C:\sandbox-utils\utils\curl-tmp-dl.bat "https://go.microsoft.com/fwlink/?LinkId=708343" StorageExplorer.exe
+call C:\sandbox-utils\utils\curl-tmp-dl.bat "https://go.microsoft.com/fwlink/?LinkId=708343" StorageExplorer.exe
 @rem C:\sandbox-tmp\StorageExplorer.exe /VERYSILENT /NORESTART /ALLUSERS
 C:\sandbox-tmp\StorageExplorer.exe /VERYSILENT /NORESTART /CURRENTUSER
--- app-script/jq-install-on-the-fly.bat
+++ app-script/jq-install-on-the-fly.bat
@@ -1,3 +1,3 @@
-C:\sandbox-utils\utils\curl-tmp-dl.bat "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-win64.exe" jq-win64.exe
+call C:\sandbox-utils\utils\curl-tmp-dl.bat "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-win64.exe" jq-win64.exe
 xcopy /Y /F C:\sandbox-tmp\jq-win64.exe C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps
 move /Y C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps\jq-win64.exe C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps\jq.exe

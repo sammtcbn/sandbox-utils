cf38023f6a145789e1378b9266ccecc277c50d14
sammtcbn <sammtcbn@gmail.com>
2021-03-11 18:32:39 +0800

    minor update

--- app-script/nircmd-install-on-the-fly.bat
+++ app-script/nircmd-install-on-the-fly.bat
@@ -1,5 +1,4 @@
-mkdir C:\sandbox-tmp
-curl --output C:\sandbox-tmp\nircmd-x64.zip -L https://www.nirsoft.net/utils/nircmd-x64.zip
+call C:\sandbox-utils\utils\curl-tmp-dl.bat "https://www.nirsoft.net/utils/nircmd-x64.zip" nircmd-x64.zip
 call C:\sandbox-utils\utils\zip-extract.bat C:\sandbox-tmp\nircmd-x64.zip C:\sandbox-tmp
 xcopy /Y C:\sandbox-tmp\nircmd.exe C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps
 xcopy /Y C:\sandbox-tmp\nircmdc.exe C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps
--- app-script/nircmd-volume-highest.bat
+++ app-script/nircmd-volume-highest.bat
@@ -11,12 +11,12 @@ if /I %ERRORLEVEL% NEQ 0 (
         C:\ProgramData\chocolatey\lib\nircmd\tools\nircmd.exe setsysvolume 65535
     ) else (
         @rem file doesn't exist
-        echo nircmd not found in chocolatey folder C:\sandbox-tmp\sandbox-utils-log.txt
+        echo nircmd not found in chocolatey folder >> C:\sandbox-tmp\sandbox-utils-log.txt
         goto :EOF
     )
 
 ) else (
-    echo nircmd found in path C:\sandbox-tmp\sandbox-utils-log.txt
+    echo nircmd found in path >> C:\sandbox-tmp\sandbox-utils-log.txt
     nircmd setsysvolume 65535
 )
 

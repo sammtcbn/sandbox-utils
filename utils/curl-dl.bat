901a93f210745183262e12975b20aa20a1f36ea3
sammtcbn <sammtcbn@gmail.com>
2021-03-13 00:30:54 +0800

    not to download if file exists

--- utils/curl-dl.bat
+++ utils/curl-dl.bat
@@ -3,9 +3,17 @@ set url=%1
 set targetFileName=%2
 call C:\sandbox-utils\utils\is-in-wsb.bat
 IF %ERRORLEVEL% EQU 0 (
-  call C:\sandbox-utils\utils\create-folder.bat C:\sandbox-tmp
-  curl --output C:\sandbox-tmp\%targetFileName% -L %url%
+  if not exist C:\sandbox-tmp\%targetFileName% (
+    call C:\sandbox-utils\utils\create-folder.bat C:\sandbox-tmp
+    curl --output C:\sandbox-tmp\%targetFileName% -L %url%
+  ) else (
+    echo C:\sandbox-tmp\%targetFileName% exist
+  )
 ) else (
-  call C:\sandbox-utils\utils\create-folder.bat C:\sandbox-dl
-  curl --output C:\sandbox-dl\%targetFileName% -L %url%
+  if not exist C:\sandbox-dl\%targetFileName% (
+    call C:\sandbox-utils\utils\create-folder.bat C:\sandbox-dl
+    curl --output C:\sandbox-dl\%targetFileName% -L %url%
+  ) else (
+    echo C:\sandbox-dl\%targetFileName% exist
+  )
 )

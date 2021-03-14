97e996179ff8bee290e35f508d7710fbdf50d212
sammtcbn <sammtcbn@gmail.com>
2021-03-14 12:31:08 +0800

    minor update

--- wsb-archive/nodejs-dev-init.bat
+++ wsb-archive/nodejs-dev-init.bat
@@ -1,6 +1,8 @@
-call C:\sandbox-utils\app-script\edge-shortcut-remove.bat
+call C:\sandbox-utils\app-script\msys2-favorite-binary-download.bat
 call C:\sandbox-utils\app-script\7zip-download.bat
 call C:\sandbox-utils\app-script\npp-download.bat
+call C:\sandbox-utils\app-script\chrome-download.bat
 call C:\sandbox-utils\app-script\git-download.bat
+call C:\sandbox-utils\app-script\sam-git-utils-download.bat
 call C:\sandbox-utils\app-script\nodejs-download.bat
 call C:\sandbox-utils\app-script\vscode-download.bat

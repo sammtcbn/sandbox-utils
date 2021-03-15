66f636fae4abeb56c56e7c19c199c30d49944d08
sammtcbn <sammtcbn@gmail.com>
2021-03-15 10:14:22 +0800

    NotePad++ update to 7.9.4

--- app-script/npp-download.bat
+++ app-script/npp-download.bat
@@ -1,6 +1,6 @@
 @echo off
-set URL=https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.3
-set FN=npp.7.9.3.Installer.x64.exe
+set URL=https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.4
+set FN=npp.7.9.4.Installer.x64.exe
 set FULLURL="%URL%/%FN%"
 set OUT=%FN%
 call C:\sandbox-utils\utils\curl-dl.bat "%FULLURL%" %OUT%

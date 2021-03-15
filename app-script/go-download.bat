24502947cbe2c5bfd634fe3e1426d90577076091
sammtcbn <sammtcbn@gmail.com>
2021-03-15 09:59:28 +0800

    go update to 1.16.2

--- app-script/go-download.bat
+++ app-script/go-download.bat
@@ -1,6 +1,6 @@
 @echo off
 set URL=https://golang.org/dl
-set FN=go1.16.windows-amd64.msi
+set FN=go1.16.2.windows-amd64.msi
 set FULLURL="%URL%/%FN%"
 set OUT=%FN%
 call C:\sandbox-utils\utils\curl-dl.bat "%FULLURL%" %OUT%

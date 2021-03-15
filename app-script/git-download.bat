7e2e01f29af4152de90a79e165aea76ad08029ea
sammtcbn <sammtcbn@gmail.com>
2021-03-15 09:56:54 +0800

    git update to 2.30.2

--- app-script/git-download.bat
+++ app-script/git-download.bat
@@ -1,6 +1,6 @@
 @echo off
-set URL=https://github.com/git-for-windows/git/releases/download/v2.30.1.windows.1
-set FN=Git-2.30.1-64-bit.exe
+set URL=https://github.com/git-for-windows/git/releases/download/v2.30.2.windows.1
+set FN=Git-2.30.2-64-bit.exe
 set FULLURL="%URL%/%FN%"
 set OUT=%FN%
 call C:\sandbox-utils\utils\curl-dl.bat "%FULLURL%" %OUT%
--- app-script/git-install.bat
+++ app-script/git-install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set FN=Git-2.30.1-64-bit.exe
+set FN=Git-2.30.2-64-bit.exe
 
 if exist C:\sandbox-dl\%FN% (
   set dlpath=C:\sandbox-dl

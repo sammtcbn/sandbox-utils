996a6f36983be9fceacb3ad82eb030184b1d9fc8
sammtcbn <sammtcbn@gmail.com>
2021-03-16 11:51:54 +0800

    minor update

--- wsb-by-chocolatey/pure.bat
+++ wsb-by-chocolatey/pure.bat
@@ -1,4 +1,10 @@
+@rem start
 call C:\sandbox-utils\utils\init.bat
 call C:\sandbox-utils\utils\wait-network-ready.bat
+call C:\sandbox-utils\app-script\edge-shortcut-remove.bat
+
+@rem chocolatey
 call C:\sandbox-utils\app-script\chocolatey-install.bat
+
+@rem end
 call C:\sandbox-utils\utils\postaction.bat

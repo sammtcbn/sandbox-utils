2d17a1490e6a9f2831d975699f252c59f38327a4
sammtcbn <sammtcbn@gmail.com>
2021-03-19 00:45:16 +0800

    minor update

--- app-script/brave-incognito-openurl.bat
+++ app-script/brave-incognito-openurl.bat
@@ -1,6 +1,9 @@
 @echo off
 @rem ex: start brave --incognito "https://www.google.com/"
 if "%~1"=="" goto labelexit
+
+@rem start brave --start-maximized --incognito %1
+
 start brave --incognito %1
 
 :labelexit
--- app-script/chrome-incognito-openurl.bat
+++ app-script/chrome-incognito-openurl.bat
@@ -1,6 +1,9 @@
 @echo off
 @rem ex: start chrome --incognito "https://www.google.com/"
 if "%~1"=="" goto labelexit
+
+@rem start chrome -start-maximized --incognito %1
+
 start chrome --incognito %1
 
 :labelexit

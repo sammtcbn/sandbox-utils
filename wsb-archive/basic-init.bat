e07b5b9a9254acad59cfc33e68f390d9fbe57a3a
sammtcbn <sammtcbn@gmail.com>
2021-03-18 18:31:10 +0800

    combine all download script

--- wsb-archive/basic-init.bat
+++
@@ -1,7 +0,0 @@
-call C:\sandbox-utils\app-script\msys2-favorite-binary-download.bat
-call C:\sandbox-utils\app-script\7zip-download.bat
-call C:\sandbox-utils\app-script\npp-download.bat
-call C:\sandbox-utils\app-script\chrome-download.bat
-call C:\sandbox-utils\app-script\git-download.bat
-call C:\sandbox-utils\app-script\sam-git-utils-download.bat
-call C:\sandbox-utils\app-script\setdefaultbrowser-download.bat
--- wsb-archive/mosquitto-init.bat
+++
@@ -1,2 +0,0 @@
-call C:\sandbox-utils\app-script\vc-redist-2015-2019-x64-download.bat
-call C:\sandbox-utils\app-script\mosquitto-download.bat

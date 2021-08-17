cb60d22f41110541cac3c640fac2cbe763078804
sammtcbn <sammtcbn@gmail.com>
2021-08-17 23:26:31 +0800

    node.js v12 change to 12.22.5

--- app-script/nodejs-v12-download.bat
+++ app-script/nodejs-v12-download.bat
@@ -1,6 +1,6 @@
 @echo off
-set URL=https://nodejs.org/dist/v12.22.4
-set FN=node-v12.22.4-x64.msi
+set URL=https://nodejs.org/dist/v12.22.5
+set FN=node-v12.22.5-x64.msi
 set FULLURL="%URL%/%FN%"
 set OUT=%FN%
 call C:\sandbox-utils\utils\curl-dl.bat "%FULLURL%" %OUT%

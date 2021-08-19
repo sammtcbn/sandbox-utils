3bacb783573911bdd548dee1b08f072f889e1b6b
sammtcbn <sammtcbn@gmail.com>
2021-08-19 14:34:14 +0800

    VSCode change to v1.59.0

--- app-script/vscode-download.bat
+++ app-script/vscode-download.bat
@@ -1,8 +1,8 @@
 @echo off
 @rem set URL=
 @rem set FN=
-set FULLURL=https://update.code.visualstudio.com/1.58.2/win32-x64-user/stable
-set OUT=VSCodeUserSetup-x64-1.58.2.exe
+set FULLURL=https://update.code.visualstudio.com/1.59.0/win32-x64-user/stable
+set OUT=VSCodeUserSetup-x64-1.59.0.exe
 call C:\sandbox-utils\utils\curl-dl.bat "%FULLURL%" %OUT%
 
 @rem download url refer to https://code.visualstudio.com/updates/

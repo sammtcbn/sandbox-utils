f1899737d2b76e61d0f25ca668f04285c966adbd
sammtcbn <sammtcbn@gmail.com>
2021-08-24 01:13:34 +0800

    golang change to v1.17

--- app-script/go-download.bat
+++ app-script/go-download.bat
@@ -1,6 +1,6 @@
 @echo off
 set URL=https://golang.org/dl
-set FN=go1.16.7.windows-amd64.msi
+set FN=go1.17.windows-amd64.msi
 set FULLURL="%URL%/%FN%"
 set OUT=%FN%
 call C:\sandbox-utils\utils\curl-dl.bat "%FULLURL%" %OUT%

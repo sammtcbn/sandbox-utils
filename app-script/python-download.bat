e85a46a0fdf208483700e4e3aeed06d3984402b0
sammtcbn <sammtcbn@gmail.com>
2021-09-09 18:05:12 +0800

    python change to v3.9.7

--- app-script/python-download.bat
+++ app-script/python-download.bat
@@ -1,6 +1,6 @@
 @echo off
-set URL=https://www.python.org/ftp/python/3.9.6
-set FN=python-3.9.6-amd64.exe
+set URL=https://www.python.org/ftp/python/3.9.7
+set FN=python-3.9.7-amd64.exe
 set FULLURL="%URL%/%FN%"
 set OUT=%FN%
 call C:\sandbox-utils\utils\curl-dl.bat "%FULLURL%" %OUT%

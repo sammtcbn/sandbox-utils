471f2a1dd66b655a54659c7f6a7a77be30f4ddde
sammtcbn <sammtcbn@gmail.com>
2021-09-09 18:03:17 +0800

    node-v16 change to v16.9.0

--- app-script/nodejs-v16-download.bat
+++ app-script/nodejs-v16-download.bat
@@ -1,6 +1,6 @@
 @echo off
-set URL=https://nodejs.org/dist/v16.6.0
-set FN=node-v16.6.0-x64.msi
+set URL=https://nodejs.org/dist/v16.9.0
+set FN=node-v16.9.0-x64.msi
 set FULLURL="%URL%/%FN%"
 set OUT=%FN%
 call C:\sandbox-utils\utils\curl-dl.bat "%FULLURL%" %OUT%
--- app-script/nodejs-v16-install.bat
+++ app-script/nodejs-v16-install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set FN=node-v16.6.0-x64.msi
+set FN=node-v16.9.0-x64.msi
 
 if exist C:\sandbox-dl\%FN% (
   set dlpath=C:\sandbox-dl

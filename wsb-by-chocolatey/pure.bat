830977fa5337eccd6283086f0fd7369cb13368a8
sammtcbn <sammtcbn@gmail.com>
2022-02-23 17:28:35 +0800

    re-org

--- wsb-by-chocolatey/pure.bat
+++
@@ -1,9 +0,0 @@
-@rem start
-call C:\sandbox-utils\utils\init.bat
-call C:\sandbox-utils\utils\wait-network-ready.bat
-
-@rem chocolatey
-call C:\sandbox-utils\app-script\chocolatey-install.bat
-
-@rem end
-call C:\sandbox-utils\utils\postaction.bat

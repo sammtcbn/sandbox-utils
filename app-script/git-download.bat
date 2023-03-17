c7f4c67fb48eb6b6293d632f807cf2b311dc0308
sammtcbn <sammtcbn@gmail.com>
2023-03-17 19:03:04 +0800

    remove useless files

--- app-script/git-download.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set URL=https://github.com/git-for-windows/git/releases/download/v2.33.1.windows.1
-set FN=Git-2.33.1-64-bit.exe
-set FULLURL="%URL%/%FN%"
-set OUT=%FN%
-call C:\sandbox-utils\utils\curl-dl.bat "%FULLURL%" %OUT%
--- app-script/mosquitto-download.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set URL=https://mosquitto.org/files/binary/win64
-set FN=mosquitto-2.0.12-install-windows-x64.exe
-set FULLURL="%URL%/%FN%"
-set OUT=%FN%
-call C:\sandbox-utils\utils\curl-dl.bat "%FULLURL%" %OUT%

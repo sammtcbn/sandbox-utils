491679429b247705a517395cdcf0109c722cc720
sammtcbn <sammtcbn@gmail.com>
2023-03-17 18:44:43 +0800

    new a sandbox for git

---
+++ git.init.bat
@@ -0,0 +1,1 @@
+call C:\win-app-installer\git-download.bat
---
+++ git.logon.bat
@@ -0,0 +1,10 @@
+@rem start
+call C:\sandbox-utils\utils\init.bat
+call C:\sandbox-utils\app-script\command-prompt-encoding-utf8.bat
+call C:\sandbox-utils\utils\wait-network-ready.bat
+rem call C:\win-app-installer\edge-shortcut-remove.bat
+
+call C:\win-app-installer\git-install.bat
+
+@rem end
+call C:\sandbox-utils\utils\postaction.bat

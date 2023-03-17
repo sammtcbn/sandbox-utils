0ee1b3735e54ff8aa8e75e43bb0f63e947785ad9
sammtcbn <sammtcbn@gmail.com>
2023-03-17 18:59:39 +0800

    new a sandbox for mosquitto

---
+++ mosquitto.init.bat
@@ -0,0 +1,2 @@
+call C:\win-app-installer\vc-redist-2015-2019-x64-download.bat
+call C:\win-app-installer\mosquitto-download.bat
--- mosquitto.logon.bat
+++ mosquitto.logon.bat
@@ -1,6 +1,10 @@
+@rem start
 call C:\sandbox-utils\utils\init.bat
 call C:\sandbox-utils\app-script\command-prompt-encoding-utf8.bat
 call C:\sandbox-utils\utils\wait-network-ready.bat
-call C:\sandbox-utils\app-script\vc-redist-2015-2019-x64-install.bat
-call C:\sandbox-utils\app-script\mosquitto-install.bat
+
+call C:\win-app-installer\vc-redist-2015-2019-x64-install.bat
+call C:\win-app-installer\mosquitto-install.bat
+
+@rem end
 call C:\sandbox-utils\utils\postaction.bat

c71e12a4ac48cacfdcd84fe98a8d16933a4b8cde
sam <sammtcbn@gmail.com>
2023-03-18 12:25:47 +0800

    new a pure sandbox

---
+++ pure.logon.bat
@@ -0,0 +1,7 @@
+@rem start
+call C:\sandbox-utils\utils\init.bat
+call C:\sandbox-utils\app-script\command-prompt-encoding-utf8.bat
+call C:\sandbox-utils\utils\wait-network-ready.bat
+
+@rem end
+call C:\sandbox-utils\utils\postaction.bat
--- wsb-archive/pure.bat
+++
@@ -1,5 +0,0 @@
-@rem start
-call C:\sandbox-utils\utils\init.bat
-
-@rem end
-call C:\sandbox-utils\utils\postaction.bat

0b463b72575cc6d49ee5ccc268a62bb9881fa6da
sammtcbn <sammtcbn@gmail.com>
2023-03-20 18:35:36 +0800

    add a sandbox for debugging MQTT

---
+++ mqtt-debug.init.bat
@@ -0,0 +1,10 @@
+@rem basic
+call C:\win-app-installer\msys2-favorite-binary-download.bat
+call C:\win-app-installer\7zip-download.bat
+call C:\win-app-installer\npp-download.bat
+call C:\win-app-installer\chrome-download.bat
+call C:\win-app-installer\setdefaultbrowser-download.bat
+
+@rem
+call C:\win-app-installer\mqttfx-download.bat
+
--- nodejs-v12-dev.init.bat
+++ nodejs-v12-dev.init.bat
@@ -2,8 +2,8 @@
 call C:\win-app-installer\msys2-favorite-binary-download.bat
 call C:\win-app-installer\7zip-download.bat
 call C:\win-app-installer\npp-download.bat
-call C:\win-app-installer\setdefaultbrowser-download.bat
 call C:\win-app-installer\chrome-download.bat
+call C:\win-app-installer\setdefaultbrowser-download.bat
 
 @rem development tools
 call C:\win-app-installer\git-download.bat

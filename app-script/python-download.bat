8256b0b4964bbfabd236dffe152c25efd0497728
sammtcbn <sammtcbn@gmail.com>
2023-03-20 18:23:19 +0800

    add a sandbox for python development

--- app-script/python-download.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set URL=https://www.python.org/ftp/python/3.10.0
-set FN=python-3.10.0-amd64.exe
-set FULLURL="%URL%/%FN%"
-set OUT=%FN%
-call C:\sandbox-utils\utils\curl-dl.bat "%FULLURL%" %OUT%
--- app-script/python-pkg-ins.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-if "%~1"=="" goto bye
-
-call C:\sandbox-utils\utils\folder-create.bat C:\sandbox-tmp
-
-pip install %* >> C:\sandbox-tmp\pip-log.txt
-
-:bye

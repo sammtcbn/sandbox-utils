4d7d755e205af9731b9803820bd90467c4a3cb27
sammtcbn <sammtcbn@gmail.com>
2023-03-20 18:51:41 +0800

    add a sandbox for go development

---
+++ go-dev.init.bat
@@ -0,0 +1,25 @@
+@rem basic
+call C:\win-app-installer\msys2-favorite-binary-download.bat
+call C:\win-app-installer\7zip-download.bat
+call C:\win-app-installer\npp-download.bat
+call C:\win-app-installer\chrome-download.bat
+call C:\win-app-installer\setdefaultbrowser-download.bat
+
+@rem development tools
+call C:\win-app-installer\git-download.bat
+call C:\win-app-installer\sam-git-utils-download.bat
+call C:\win-app-installer\winmerge-download.bat
+
+@rem terminal
+call C:\win-app-installer\windows-terminal-download.bat
+
+@rem python
+call C:\win-app-installer\go-download.bat
+
+@rem Visual Studio Code
+call C:\win-app-installer\vscode-download.bat
+
+@rem misc
+call C:\win-app-installer\jq-download.bat
+call C:\win-app-installer\bginfo-download.bat
+call C:\win-app-installer\bginfo-download.bat

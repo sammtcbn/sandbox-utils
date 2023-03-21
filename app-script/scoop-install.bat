406b5e2a0c47609b616adb22386c169026831b60
sammtcbn <sammtcbn@gmail.com>
2023-03-21 21:04:54 +0800

    remove useless files

--- app-script/scoop-install.bat
+++
@@ -1,1 +0,0 @@
-start /wait powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh'))"
--- wsb-by-scoop/pure.bat
+++
@@ -1,9 +0,0 @@
-@rem start
-call C:\sandbox-utils\utils\init.bat
-call C:\sandbox-utils\utils\wait-network-ready.bat
-
-@rem scoop
-call C:\sandbox-utils\app-script\scoop-install.bat
-
-@rem end
-call C:\sandbox-utils\utils\postaction.bat

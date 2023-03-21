0eb3d3d6fe27c05c513dabefed3e401edc200d07
sammtcbn <sammtcbn@gmail.com>
2023-03-21 18:35:14 +0800

    add a sandbox for anaconda

---
+++ anaconda.init.bat
@@ -0,0 +1,18 @@
+@rem basic
+call C:\win-app-installer\msys2-favorite-binary-download.bat
+call C:\win-app-installer\7zip-download.bat
+call C:\win-app-installer\npp-download.bat
+call C:\win-app-installer\chrome-download.bat
+call C:\win-app-installer\setdefaultbrowser-download.bat
+
+@rem development tools
+call C:\win-app-installer\git-download.bat
+call C:\win-app-installer\sam-git-utils-download.bat
+call C:\win-app-installer\winmerge-download.bat
+
+@rem terminal
+call C:\win-app-installer\windows-terminal-download.bat
+
+@rem
+call C:\win-app-installer\anaconda-download.bat
+
---
+++ anaconda.logon.bat
@@ -0,0 +1,27 @@
+@rem start
+call C:\sandbox-utils\utils\init.bat
+call C:\sandbox-utils\app-script\command-prompt-encoding-utf8.bat
+call C:\sandbox-utils\utils\wait-network-ready.bat
+
+@rem basic
+call C:\win-app-installer\msys2-favorite-binary-install.bat
+call C:\win-app-installer\7zip-install.bat
+call C:\win-app-installer\npp-install.bat
+call C:\win-app-installer\chrome-install.bat
+call C:\win-app-installer\chrome-shortcut-remove.bat
+call C:\win-app-installer\setdefaultbrowser-install.bat
+call C:\win-app-installer\setdefaultbrowser-chrome.bat
+
+@rem development tools
+call C:\win-app-installer\git-install.bat
+call C:\win-app-installer\sam-git-utils-install.bat
+call C:\win-app-installer\winmerge-install.bat
+
+@rem terminal
+call C:\win-app-installer\windows-terminal-install.bat
+
+@rem
+call C:\win-app-installer\anaconda-install.bat
+
+@rem end
+call C:\sandbox-utils\utils\postaction.bat

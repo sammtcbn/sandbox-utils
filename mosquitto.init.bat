ebf8ccab5188d14f245394e5a998b9e3b7a234b9
sammtcbn <sammtcbn@gmail.com>
2025-05-09 17:47:19 +0800

    mosquitto sandbox switch to vc redistributable 2015-2022

--- mosquitto.init.bat
+++ mosquitto.init.bat
@@ -1,2 +1,2 @@
-call C:\win-app-installer\vc-redist-2015-2019-x64-download.bat
+call C:\win-app-installer\vc-redist-2015-2022-x64-download.bat
 call C:\win-app-installer\mosquitto-download.bat
--- mosquitto.logon.bat
+++ mosquitto.logon.bat
@@ -3,7 +3,7 @@ call C:\sandbox-utils\utils\init.bat
 call C:\sandbox-utils\app-script\command-prompt-encoding-utf8.bat
 call C:\sandbox-utils\utils\wait-network-ready.bat
 
-call C:\win-app-installer\vc-redist-2015-2019-x64-install.bat
+call C:\win-app-installer\vc-redist-2015-2022-x64-install.bat
 call C:\win-app-installer\mosquitto-install.bat
 
 @rem end
